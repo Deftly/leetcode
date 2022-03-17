89e9a099442f51a6d345d07816e3e0f81ed8ee6a
Deftly <deftkz@gmail.com>
2022-03-17 15:04:43 -0700

    base case + more DRY code #242

--- algorithms/valid_anagram.py
+++ algorithms/valid_anagram.py
@@ -6,9 +6,10 @@
 # exactly once.
 
 def is_anagram(s, t):
+    if len(s) != len(t):
+        return False
     letters_s, letters_t = {}, {}
-    for letter in s:
-        letters_s[letter] = letters_s.get(letter, 0) + 1
-    for letter in t:
-        letters_t[letter] = letters_t.get(letter, 0) + 1
+    for i in range(len(s)):
+        letters_s[s[i]] = letters_s.get(s[i], 0) + 1
+        letters_t[t[i]] = letters_t.get(t[i], 0) + 1
     return letters_s == letters_t
