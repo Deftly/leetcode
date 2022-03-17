a6c2ae29d6703370b3f01800af0557b933d45631
Deftly <deftkz@gmail.com>
2022-03-17 15:11:01 -0700

    Alternative approach #242

--- algorithms/valid_anagram.py
+++ algorithms/valid_anagram.py
@@ -1,9 +1,12 @@
-# Given two strings s and t, return true if t is an anagram of s, and 
+# Given two strings s and t, return true if t is an anagram of s, and
 # false otherwise.
 #
-# An Anagram is a word or phrase formed by rearranging the letters of a 
-# different word or phrase, typically using all the original letters 
+# An Anagram is a word or phrase formed by rearranging the letters of a
+# different word or phrase, typically using all the original letters
 # exactly once.
+
+from typing import Counter
+
 
 def is_anagram(s, t):
     if len(s) != len(t):
@@ -13,3 +16,7 @@ def is_anagram(s, t):
         letters_s[s[i]] = letters_s.get(s[i], 0) + 1
         letters_t[t[i]] = letters_t.get(t[i], 0) + 1
     return letters_s == letters_t
+
+
+def is_anagram_v2(s, t):
+    return Counter(s) == Counter(t)
